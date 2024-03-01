7da26b2b3c14dd5b0a3f46b9133d8abde2e82b09
michaelessiet <emsaa2002@gmail.com>
2024-03-01 01:18:04 +0100

    pkgx details display

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,10 @@ import type { PkgxIndexRes } from "#imports"
 
 let pkgList = (await pkgxClient.get<PkgxIndexRes[]>("/index.json")).data
 pkgList.forEach((item) => {
-  item.url = item.url.replace("https://pkgx.dev/pkgs/", "")
+	item.url = item.url.replace("https://pkgx.dev/pkgs/", "")
 })
 pkgList = pkgList.filter((item) => {
-  return item.type === "pkg"
+	return item.type === "pkg"
 })
 </script>
 
@@ -16,15 +16,21 @@ pkgList = pkgList.filter((item) => {
 		<h1>Pkgx Registry</h1>
 
 		<div class="flex flex-col gap-2">
-			<h2>What's new?</h2>
 			<div v-for="(item, index) in pkgList" :key="item.url">
-        <NuxtLink :to="'/pkg/'+convertSlashToColon(item.url)">
-				<div class="rounded-md border-2 p-2 border-[#1B2F4E]">
-					<h3>{{ item.title }}</h3>
-					<p>{{ item.description }}</p>
-				</div>
-        </NuxtLink>
+				<NuxtLink :to="'/pkg/' + convertSlashToColon(item.url)">
+					<div class="rounded-md border-2 p-2 border-[#1B2F4E]">
+						<h3>{{ item.title }}</h3>
+						<p>{{ item.description }}</p>
+					</div>
+				</NuxtLink>
 			</div>
 		</div>
+
+		<button
+			class="fixed bottom-8 right-8 bg-blue-900 rounded-lg p-2"
+			onclick="window.scrollTo({top:0, behavior: 'smooth'})"
+		>
+			To top
+		</button>
 	</div>
 </template>
--- pages/pkg/[id].vue
+++ pages/pkg/[id].vue
@@ -1,22 +1,95 @@
 <script lang="ts" setup>
 import { pkgxV1Client } from "#imports"
-import axios from "axios";
-import {marked} from "marked";
+import axios from "axios"
+import { marked } from "marked"
+import yaml from "js-yaml"
 
 const route = useRoute()
+
 const pkgData = (
 	await pkgxV1Client.get<PkgxPackageRes>(`/packages/${route.params.id}`)
 ).data
-const [ghUsername, ghReponame] = pkgData.github_url.replace("https://github.com/", "").split('/')
-const readmeData = (await githubApiClient.get(`/repos/${ghUsername}/${ghReponame}/readme`)).data
-const readmeMarkdown = (await axios.get(readmeData.download_url)).data
+const pkgDataYamlPath = pkgData.package_yml_url.replace(
+	"https://github.com/teaxyz/pantry/blob/main/projects/",
+	""
+)
+
+const [ghUsername, ghReponame] = pkgData.github_url
+	.replace("https://github.com/", "")
+	.split("/")
+
+const [readmeData, yamlData] = await Promise.all([
+	githubApiClient.get(`/repos/${ghUsername}/${ghReponame}/readme`),
+	axios.get(
+		`https://raw.githubusercontent.com/pkgxdev/pantry/main/projects/${pkgDataYamlPath}`
+	),
+])
+
+const pkgYaml = yaml.load(yamlData.data)
+const dependencies = Object.entries(pkgYaml.dependencies ?? {})
+const programs: string[] = pkgYaml.provides
+const companions = Object.entries(pkgYaml.companions ?? {})
+const versions = Array.from(
+	new Set(pkgData.bottles.map((bottle) => bottle.version))
+)
+const runCommand = `sh <(curl https://pkgx.sh) +${pkgData.full_name} sh`
+
+const readmeMarkdown = (await axios.get(readmeData.data.download_url)).data
 const renderedHtml = await marked.parse(readmeMarkdown)
 </script>
 
 <template>
-	<div class="text-white p-8 bg-[#070C14]">
+  <Head>
+    <title>{{ pkgData.full_name }}</title>
+  </Head>
+
+	<div class="text-white p-8 min-h-screen bg-[#070C14]">
 		<h1>{{ pkgData.full_name }}</h1>
-		<h2>{{ JSON.stringify(pkgData, null, 2) }}</h2>
-    <div v-html="renderedHtml"/>
+    <br>
+		<div class="flex flex-row max-md:flex-col-reverse gap-2">
+			<div
+				class="border-2 rounded-lg p-4 border-[#1B2F4E]"
+				v-html="renderedHtml"
+			/>
+			<div
+				class="flex flex-col gap-2 border-2 rounded-lg p-4 border-[#1B2F4E] min-w-[30%]"
+			>
+				<h2>pkgx info</h2>
+
+				<div class="bg-gray-800 text-white p-4 rounded-lg shadow-md">
+					<code class="block whitespace-pre-wrap">
+						{{ runCommand }}
+					</code>
+				</div>
+
+				<h3>Programs</h3>
+				<ul class="list-disc mx-6">
+					<li v-for="(item, index) in programs" :key="item">
+						{{ item }}
+					</li>
+				</ul>
+
+				<h3>Companions</h3>
+				<ul class="list-disc mx-6">
+					<li v-for="(item, index) in companions" :key="item[0]">
+						{{ item[0] }}
+					</li>
+				</ul>
+
+				<h3>Dependencies</h3>
+				<ul class="list-disc mx-6">
+					<li v-for="(item, index) in dependencies" :key="item[0]">
+						<NuxtLink class="text-blue-300" :to="`/pkg/${item[0]}`"> {{ item[0] }}: {{ item[1] }} </NuxtLink>
+					</li>
+				</ul>
+
+        <h3>Versions</h3>
+        <ul class="list-disc mx-6">
+          <li v-for="(item, index) in versions" :key="item">
+            {{ item }}
+          </li>
+        </ul>
+			</div>
+		</div>
 	</div>
 </template>
